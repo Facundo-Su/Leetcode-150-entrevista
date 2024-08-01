76e438c441c5f6c16ad0e51cf83be2ffb1a84763
Facundo <facundosu26@gmail.com>
2024-08-01 01:04:32 -0300

    ejercicio 121 terminado

--- src/main/java/org/example/BestTimeToBuyAndSell_121.java
+++ src/main/java/org/example/BestTimeToBuyAndSell_121.java
@@ -4,14 +4,28 @@ public class BestTimeToBuyAndSell_121 {
 
     public int maxProfit(int[] prices){
 
-        int max_ganancia = ;
+        int max_ganancia=0;
+        int valor_minimo = Integer.MAX_VALUE;
 
         for(int num : prices){
+
+            if(num<valor_minimo){
+                valor_minimo= num;
+            }else{
+                int ganancia = num - valor_minimo;
+
+                if(ganancia >max_ganancia){
+
+                    max_ganancia = ganancia;
+                }
+
+            }
+
 
         }
 
 
-        return;
+        return max_ganancia;
     }
 
 }
