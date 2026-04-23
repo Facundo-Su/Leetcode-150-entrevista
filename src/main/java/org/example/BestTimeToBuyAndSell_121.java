a59c6e8666c3dd082708943a0a941852b15a6bfd
Facundo <facundosu26@gmail.com>
2026-04-23 02:16:00 -0300

    Leetcode

--- src/main/java/org/example/BestTimeToBuyAndSell_121.java
+++ src/main/java/org/example/BestTimeToBuyAndSell_121.java
@@ -4,28 +4,21 @@ public class BestTimeToBuyAndSell_121 {
 
     public int maxProfit(int[] prices){
 
-        int max_ganancia=0;
-        int valor_minimo = Integer.MAX_VALUE;
+        int cantidad = prices.length;
+        int profitMax =0;
+        int precioMin=prices[0];
 
-        for(int num : prices){
-
-            if(num<valor_minimo){
-                valor_minimo= num;
-            }else{
-                int ganancia = num - valor_minimo;
-
-                if(ganancia >max_ganancia){
-
-                    max_ganancia = ganancia;
-                }
-
+        for(int i=1;i<cantidad;i++){
+            if(prices[i]<precioMin){
+                precioMin=prices[i];
             }
-
+            if(profitMax<prices[i]-precioMin){
+                profitMax = prices[i] - precioMin;
+            }
 
         }
 
-
-        return max_ganancia;
+        return profitMax;
     }
 
 }
--- src/main/java/org/example/RotateArray189.java
+++ src/main/java/org/example/RotateArray189.java
@@ -3,17 +3,21 @@ package org.example;
 public class RotateArray189 {
     public void rotate(int[] nums, int k) {
 
-        int puntero;
-        int cantidad= nums.length;
-        int aux;
-        for(int i=0;i<k;i++){
-            nums[0] = nums[cantidad-1];
+        k= k% nums.length;
 
-            for (int j=cantidad-2;j>1;j--){
-                aux=nums[j+1];
-                nums[j+1]=nums[j];
-            }
+        invertir(nums,0, nums.length-1);
+        invertir(nums,0,k-1);
+        invertir(nums,k, nums.length-1);
+    }
 
+    public void invertir(int[] nums, int inicio, int fin){
+
+        while(inicio<fin){
+            int aux = nums[fin];
+            nums[fin]= nums[inicio];
+            nums[inicio]=aux;
+            inicio++;
+            fin--;
         }
 
 
